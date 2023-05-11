2b17f92690e360cb531d33219ec8267c187701f4
Ismail Pelaseyed <homanp@gmail.com>
2023-05-11 23:32:43 +0200

    Add `ApiToken` model for accessing Agents (#15)
    * Add `ApiToken` model
    
    * Fix linting issues

--- app/routers.py
+++ app/routers.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api import agents, auth, users
+from app.api import agents, api_tokens, auth, users
 
 router = APIRouter()
 api_prefix = "/api/v1"
@@ -8,3 +8,4 @@ api_prefix = "/api/v1"
 router.include_router(agents.router, tags=["Agent"], prefix=api_prefix)
 router.include_router(auth.router, tags=["Auth"], prefix=api_prefix)
 router.include_router(users.router, tags=["User"], prefix=api_prefix)
+router.include_router(api_tokens.router, tags=["Api token"], prefix=api_prefix)
