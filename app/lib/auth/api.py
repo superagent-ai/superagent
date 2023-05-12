4e3dbf7a0836019adcd4601e2d65a5074cd3dcf5
Ismail Pelaseyed <homanp@gmail.com>
2023-05-12 14:04:47 +0200

    Guard agent invocation with `X_SUPERAGENT_API_KEY`

--- app/lib/auth/api.py
+++ app/lib/auth/api.py
@@ -1,15 +1,20 @@
-from decouple import config
 from fastapi import HTTPException, Security
 from fastapi.security.api_key import APIKeyHeader
 from starlette.status import HTTP_403_FORBIDDEN
+
+from app.lib.prisma import prisma
 
 api_key_header = APIKeyHeader(name="X_SUPERAGENT_API_KEY", auto_error=False)
 
 
 async def get_api_key(api_key_header: str = Security(api_key_header)):
-    if api_key_header == config("API_KEY"):
+    try:
+        await prisma.apitoken.find_first(where={"token": api_key_header})
+
         return api_key_header
-    else:
+
+    except Exception as e:
+        print(e)
         raise HTTPException(
             status_code=HTTP_403_FORBIDDEN, detail="Could not validate API KEY"
         )
