a9cf1d0d37ac8b13acfaf0667f2fbbdcad58816f
Ismail Pelaseyed <homanp@gmail.com>
2023-05-24 11:17:24 +0200

    Fix linting issues

--- app/lib/agents.py
+++ app/lib/agents.py
@@ -9,7 +9,7 @@ from langchain.chains.conversational_retrieval.prompts import (
 from langchain.chains.question_answering import load_qa_chain
 from langchain.chat_models import ChatAnthropic, ChatOpenAI
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.llms import OpenAI, Cohere
+from langchain.llms import Cohere, OpenAI
 from langchain.memory import ChatMessageHistory, ConversationBufferMemory
 from langchain.vectorstores.pinecone import Pinecone
 
