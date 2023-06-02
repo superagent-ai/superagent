49b4b5e56bb63738d026a95fdfcd214bd67fc597
Ismail Pelaseyed <homanp@gmail.com>
2023-06-02 16:50:04 +0200

    "Small tweak"

--- app/lib/prompts.py
+++ app/lib/prompts.py
@@ -47,7 +47,7 @@ Human: {human_input}
 Assitant:
 """
 
-agent_template = """Answer the following questions as best you can You have access to the following tools:
+agent_template = """Answer the following questions as best you can. You have access to the following tools:
 
 {tools}
 
