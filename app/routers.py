9e5acb64fd4730b8ce3b665b956f30df01bb886a
Ismail Pelaseyed <homanp@gmail.com>
2023-06-18 16:20:54 +0200

    Add API endpoints for agent documents

--- app/routers.py
+++ app/routers.py
@@ -1,11 +1,24 @@
 from fastapi import APIRouter
 
-from app.api import agents, api_tokens, auth, documents, prompts, tools, traces, users
+from app.api import (
+    agents,
+    agent_documents,
+    api_tokens,
+    auth,
+    documents,
+    prompts,
+    tools,
+    traces,
+    users,
+)
 
 router = APIRouter()
 api_prefix = "/api/v1"
 
 router.include_router(agents.router, tags=["Agent"], prefix=api_prefix)
+router.include_router(
+    agent_documents.router, tags=["Agent documents"], prefix=api_prefix
+)
 router.include_router(auth.router, tags=["Auth"], prefix=api_prefix)
 router.include_router(users.router, tags=["User"], prefix=api_prefix)
 router.include_router(api_tokens.router, tags=["Api token"], prefix=api_prefix)
