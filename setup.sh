3f952b1a4829ad0d8fbed6be002e64d6fd57473f
Ismail Pelaseyed <homanp@gmail.com>
2023-07-03 23:14:40 +0200

    Update README

--- setup.sh
+++ setup.sh
@@ -4,16 +4,6 @@
 virtualenv venv
 source venv/bin/activate
 
-# Read the variable names from the `env.example` file
-for var in $(cat .env.example); do
-  # Ask the user for input
-  echo "Please provide the value for $var:"
-  read -r value
-
-  # Save the value to the `.env` file
-  echo "$var$value" >> .env
-done
-
 # Install dependencies using Poetry
 poetry install
 
