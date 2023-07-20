16456bbe20c6b0c30e11177c4476f35b6989b3b5
Ismail Pelaseyed <homanp@gmail.com>
2023-07-20 21:00:46 +0000

    Small tweaks

--- ui/app/agents/[agentId]/client-page.js
+++ ui/app/agents/[agentId]/client-page.js
@@ -224,7 +224,7 @@ export default function AgentDetailClientPage({
     onOpen: onToolModalOpen,
   } = useDisclosure();
   const {
-    isOpen: isTagModalOpan,
+    isOpen: isTagModalOpen,
     onClose: onTagModalClose,
     onOpen: onTagModalOpen,
   } = useDisclosure();
