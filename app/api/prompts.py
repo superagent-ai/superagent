ab61ae2dd3a79a571ccea468f189239c28f2ba1c
Daniel Ransom <dantheman0207@users.noreply.github.com>
2023-07-27 19:56:23 +0200

    add error logging (#215)
    * add error logging
    
    * format
    
    * format
    
    * fix import
    
    * fix error logging format
    
    * Add dependencies
    
    ---------
    
    Co-authored-by: Ismail Pelaseyed <homanp@gmail.com>

--- app/api/prompts.py
+++ app/api/prompts.py
@@ -1,10 +1,13 @@
 import json
+import logging
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 
 from app.lib.auth.prisma import JWTBearer, decodeJWT
 from app.lib.models.prompt import Prompt
 from app.lib.prisma import prisma
+
+logger = logging.getLogger(__name__)
 
 router = APIRouter()
 
@@ -12,32 +15,41 @@ router = APIRouter()
 @router.post("/prompts", name="Create a prompt", description="Create a new prompt")
 async def create_prompt(body: Prompt, token=Depends(JWTBearer())):
     """Create prompt endpoint"""
-    decoded = decodeJWT(token)
-
-    prompt = prisma.prompt.create(
-        {
-            "name": body.name,
-            "input_variables": json.dumps(body.input_variables),
-            "template": body.template,
-            "userId": decoded["userId"],
-        },
-        include={"user": True},
+    try:
+        decoded = decodeJWT(token)
+        prompt = prisma.prompt.create(
+            {
+                "name": body.name,
+                "input_variables": json.dumps(body.input_variables),
+                "template": body.template,
+                "userId": decoded["userId"],
+            },
+            include={"user": True},
+        )
+        return {"success": True, "data": prompt}
+    except Exception as e:
+        logger.error(e)
+    raise HTTPException(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
     )
-
-    return {"success": True, "data": prompt}
 
 
 @router.get("/prompts", name="List prompts", description="List all prompts")
 async def read_prompts(token=Depends(JWTBearer())):
     """List prompts endpoint"""
-    decoded = decodeJWT(token)
-    prompts = prisma.prompt.find_many(
-        where={"userId": decoded["userId"]},
-        include={"user": True},
-        order={"createdAt": "desc"},
-    )
-
-    return {"success": True, "data": prompts}
+    try:
+        decoded = decodeJWT(token)
+        prompts = prisma.prompt.find_many(
+            where={"userId": decoded["userId"]},
+            include={"user": True},
+            order={"createdAt": "desc"},
+        )
+        return {"success": True, "data": prompts}
+    except Exception as e:
+        logger.error("Couldn't find prompts for user", exc_info=e)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
 
 
 @router.get(
@@ -47,9 +59,16 @@ async def read_prompts(token=Depends(JWTBearer())):
 )
 async def read_prompt(promptId: str, token=Depends(JWTBearer())):
     """Get prompt endpoint"""
-    prompt = prisma.prompt.find_unique(where={"id": promptId}, include={"user": True})
-
-    return {"success": True, "data": prompt}
+    try:
+        prompt = prisma.prompt.find_unique(
+            where={"id": promptId}, include={"user": True}
+        )
+        return {"success": True, "data": prompt}
+    except Exception as e:
+        logger.error("Couldn't find prompt", exc_info=e)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
 
 
 @router.delete(
@@ -59,9 +78,14 @@ async def read_prompt(promptId: str, token=Depends(JWTBearer())):
 )
 async def delete_prompt(promptId: str, token=Depends(JWTBearer())):
     """Delete prompt endpoint"""
-    prisma.prompt.delete(where={"id": promptId})
-
-    return {"success": True, "data": None}
+    try:
+        prisma.prompt.delete(where={"id": promptId})
+        return {"success": True, "data": None}
+    except Exception as e:
+        logger.error("Couldn't delete prompt with id {promptId}", exc_info=e)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
 
 
 @router.patch(
@@ -69,13 +93,17 @@ async def delete_prompt(promptId: str, token=Depends(JWTBearer())):
 )
 async def patch_prompt(promptId: str, body: dict, token=Depends(JWTBearer())):
     """Patch prompt endpoint"""
-    input_variables = body["input_variables"]
-    if input_variables or input_variables == []:
-        body["input_variables"] = json.dumps(input_variables)
-
-    prompt = prisma.prompt.update(
-        data=body,
-        where={"id": promptId},
-    )
-
-    return {"success": True, "data": prompt}
+    try:
+        input_variables = body["input_variables"]
+        if input_variables or input_variables == []:
+            body["input_variables"] = json.dumps(input_variables)
+        prompt = prisma.prompt.update(
+            data=body,
+            where={"id": promptId},
+        )
+        return {"success": True, "data": prompt}
+    except Exception as e:
+        logger.error("Couldn't patch prompt with id {promptId}", exc_info=e)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
