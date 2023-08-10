8b042ff5a640e773a4e764638675f74c151b589e
Ismail Pelaseyed <homanp@gmail.com>
2023-08-10 16:58:36 +0200

    Fix issue with agent being listed but not public

--- ui/app/share/page.js
+++ ui/app/share/page.js
@@ -32,7 +32,7 @@ export default async function Share({ searchParams }) {
   const apiToken = decryptToken(token);
   const { data: agent } = await getAgent(agentId, apiToken);
 
-  if (!agent.isPublic) {
+  if (!agent.isPublic && !agent.isListed) {
     redirect("/login");
   }
 
