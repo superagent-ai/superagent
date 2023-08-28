a32a82f6481637fbf231891b11dce9ae4a306297
Ismail Pelaseyed <homanp@gmail.com>
2023-08-28 22:20:24 +0200

    Add data loaders

--- v2/app/datasource/loader.py
+++ v2/app/datasource/loader.py
@@ -1,7 +1,17 @@
 import requests
+import tempfile
 
+from urllib.parse import urlparse
 from typing import Any
-from langchain.document_loaders import TextLoader
+from langchain.document_loaders import (
+    GitLoader,
+    TextLoader,
+    PyPDFLoader,
+    UnstructuredMarkdownLoader,
+    WebBaseLoader,
+    YoutubeLoader,
+)
+from llama_index import download_loader
 from prisma.models import Datasource
 from tempfile import NamedTemporaryFile
 
@@ -23,6 +33,10 @@ class DataLoader:
             return self.load_webpage()
         elif self.datasource.type == "NOTION":
             return self.load_notion()
+        elif self.datasource.type == "YOUTUBE":
+            return self.load_youtube()
+        elif self.datasource.type == "URL":
+            return self.load_url()
         else:
             raise ValueError(f"Unsupported datasource type: {self.datasource.type}")
 
@@ -35,21 +49,52 @@ class DataLoader:
             return loader.load_and_split()
 
     def load_pdf(self):
-        # Implement PDF loading logic here
-        pass
+        loader = PyPDFLoader(file_path=self.datasource.url)
+        return loader.load_and_split()
 
     def load_markdown(self):
-        # Implement Markdown loading logic here
-        pass
+        file_response = requests.get(self.datasource.url).text
+        if file_response:
+            with NamedTemporaryFile(suffix=".md", delete=True) as temp_file:
+                temp_file.write(file_response.encode())
+                temp_file.flush()
+                loader = UnstructuredMarkdownLoader(file_path=temp_file.name)
+                return loader.load()
 
     def load_github(self):
-        # Implement Github repository loading logic here
-        pass
+        parsed_url = urlparse(self.datasource.url)
+            path_parts = parsed_url.path.split("/")  # type: ignore
+            repo_name = path_parts[2]
+
+            with tempfile.TemporaryDirectory() as temp_dir:
+                repo_path = f"{temp_dir}/{repo_name}/"  # type: ignore
+                loader = GitLoader(
+                    clone_url=self.datasource.url,
+                    repo_path=repo_path,
+                    branch=metadata["branch"],  # type: ignore
+                )
+                return loader.load_and_split()
 
     def load_webpage(self):
-        # Implement webpage loading logic here
+        RemoteDepthReader = download_loader("RemoteDepthReader")
+        depth = int(self.datasource.metadata.get("depth"))
+        loader = RemoteDepthReader(depth=depth)
+        return loader.load_langchain_documents(url=self.datasource.url)
         pass
 
     def load_notion(self):
-        # Implement Notion loading logic here
-        pass
+        NotionPageReader = download_loader("NotionPageReader")
+        integration_token = self.datasource.metadata["integration_token"]
+        page_ids = self.datasource.metadata["page_ids"]
+        loader = NotionPageReader(integration_token=integration_token)
+        return loader.load_langchain_documents(page_ids=page_ids.split(","))
+    
+    def load_youtube(self):
+        video_id = self.datasource.url.split("youtube.com/watch?v=")[-1]
+        loader = YoutubeLoader(video_id=video_id)
+        return loader.load_and_split()
+
+    def load_url(self):
+        url_list = self.datasource.url.split(",")
+        loader = WebBaseLoader(url_list)
+        return loader.load_and_split()
