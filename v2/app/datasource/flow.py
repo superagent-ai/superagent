d01a5c55abaf6b26d63003992c9b564e5381f7b9
Ismail Pelaseyed <homanp@gmail.com>
2023-08-28 14:21:39 +0200

    WIP

--- v2/app/datasource/flow.py
+++ v2/app/datasource/flow.py
@@ -1,9 +1,19 @@
 from prefect import task, flow
 from decouple import config
+from typing import Any, List
 from app.utils.prisma import prisma
-from prisma.models import Datasource
+from prisma.models import AgentDatasource, Datasource
 from llama import LLMEngine, Type, Context
 from app.datasource.loader import DataLoader
+
+VALID_FINETUNE_TYPES = [
+    "TXT",
+    "PDF",
+    "MARKDOWN",
+    "GITHUB_REPOSITORY",
+    "WEBPAGE",
+    "NOTION",
+]
 
 
 class Document(Type):
@@ -33,23 +43,35 @@ async def handle_vectorization(datasource: Datasource):
     print(f"VECTORIZE: {datasource.id}")
 
 
-VALID_INGESTION_TYPES = {
-    "TXT": handle_finetune,
-    "PDF": handle_finetune,
-    "MARKDOWN": handle_finetune,
-    "GITHUB_REPOSITORY": handle_finetune,
-    "WEBPAGE": handle_finetune,
-    "NOTION": handle_finetune,
-}
+@task
+async def handle_datasources(
+    agent_datasources: List[AgentDatasource], agent_id: str
+) -> Any:
+    llm = LLMEngine(
+        id=agent_id,
+        config={"production.key": config("LAMINI_API_KEY")},
+        model_name="chat/gpt-3.5-turbo",
+    )
+    llm.clear_data()
+    llm.save_data(
+        [
+            DataLoader(agent_datasource.datasource).load()
+            for agent_datasource in agent_datasources
+            if agent_datasource.datasource.type in VALID_FINETUNE_TYPES
+        ]
+    )
+    return llm
 
 
 @flow(name="process_datasource", description="Process new agent datasource", retries=0)
 async def process_datasource(datasource_id: str, agent_id: str):
-    datasource = await prisma.datasource.find_unique_or_raise(
-        where={"id": datasource_id}
-    )
-    handler = VALID_INGESTION_TYPES.get(datasource.type, handle_vectorization)
-    await handler(datasource=datasource)
     await prisma.agentdatasource.create(
         {"datasourceId": datasource_id, "agentId": agent_id}
     )
+    agent_datasources = await prisma.agentdatasource.find_many(
+        where={"agentId": agent_id}, include={"datasource": True}
+    )
+    callback = await handle_datasources(
+        agent_datasources=agent_datasources, agent_id=agent_id
+    )
+    print(f"CALLBACK: {callback}")
