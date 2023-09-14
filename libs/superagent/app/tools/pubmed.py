5153a7cf02cf2dd5797a6038b9b90e7baddd33a5
Ismail Pelaseyed <homanp@gmail.com>
2023-09-14 10:23:25 +0200

    Add run manager

--- libs/superagent/app/tools/pubmed.py
+++ libs/superagent/app/tools/pubmed.py
@@ -1,4 +1,9 @@
+from typing import Optional
 from langchain.tools import BaseTool, PubmedQueryRun
+from langchain.callbacks.manager import (
+    AsyncCallbackManagerForToolRun,
+    CallbackManagerForToolRun,
+)
 
 
 class PubMed(BaseTool):
@@ -6,12 +11,20 @@ class PubMed(BaseTool):
     description = "useful for answering question about medical publications"
     return_direct = False
 
-    def _run(self, search_query: str) -> str:
+    def _run(
+        self,
+        search_query: str,
+        _run_manager: Optional[CallbackManagerForToolRun] = None,
+    ) -> str:
         pubmed = PubmedQueryRun(args_schema=self.args_schema)
         output = pubmed.run(search_query)
         return output
 
-    async def _arun(self, search_query: str) -> str:
+    async def _arun(
+        self,
+        search_query: str,
+        _run_manager: Optional[AsyncCallbackManagerForToolRun] = None,
+    ) -> str:
         pubmed = PubmedQueryRun(args_schema=self.args_schema)
         output = pubmed.run(search_query)
         return output
