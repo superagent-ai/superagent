a590b9a43375e7c3b101d22ffc9f5db638eaf37b
Ismail Pelaseyed <homanp@gmail.com>
2023-09-25 13:40:55 +0200

    Add updates to Replit config files (#421)

--- libs/superagent/replit.sh
+++ libs/superagent/replit.sh
@@ -7,4 +7,4 @@ poetry install
 poetry run prisma generate
 
 # Start the application with auto-reload
-uvicorn app.main:app --reload --host 0.0.0.0 --port 8000
+gunicorn --bind :8000 --workers 2 --timeout 0  --worker-class uvicorn.workers.UvicornWorker  --threads 8 app.main:app
