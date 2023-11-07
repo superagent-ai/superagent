4c0ea83ce6ea2ea49ff3d2c8f6f7524bb577b0bc
Ismail Pelaseyed <homanp@gmail.com>
2023-11-07 22:37:32 +0100

    ADd text to speech tool using OpenAI `TTS1`  (#498)
    * Add support for TTS1 model as a tool
    
    * Fix formatting
    
    * Update documentation

---
+++ libs/superagent/app/tools/tts_1.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+from langchain.tools import BaseTool
+from openai import AsyncOpenAI, OpenAI
+
+
+class TTS1(BaseTool):
+    name = "text-to-speech"
+    description = "useful for generation voice audio from text"
+    return_direct = False
+
+    def _run(self, input: dict) -> str:
+        client = OpenAI(api_key=self.metadata["openaiApiKey"])
+        speech_file_path = Path(__file__).parent / "speech.mp3"
+        response = client.audio.speech.create(
+            model="tts-1",
+            voice=input["voice"] or "alloy",
+            input=input["text"],
+        )
+        output = response.stream_to_file(speech_file_path)
+        return output
+
+    async def _arun(self, input: dict) -> str:
+        client = AsyncOpenAI(api_key=self.metadata["openaiApiKey"])
+        speech_file_path = Path(__file__).parent / "speech.mp3"
+        response = await client.audio.speech.create(
+            model="tts-1",
+            voice=input["voice"] or "alloy",
+            input=input["text"],
+        )
+        output = response.stream_to_file(speech_file_path)
+        return output
