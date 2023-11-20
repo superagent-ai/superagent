b931da3f5449b00b87027bd78fdb113e57b2d27e
Ismail Pelaseyed <homanp@gmail.com>
2023-11-20 23:02:16 +0100

    Human feedback (#524)
    * Add human feedback buttons to chat
    
    * Small tweaks

--- libs/ui/app/agents/[agentId]/chat.tsx
+++ libs/ui/app/agents/[agentId]/chat.tsx
@@ -5,6 +5,8 @@ import { fetchEventSource } from "@microsoft/fetch-event-source"
 import dayjs from "dayjs"
 import relativeTime from "dayjs/plugin/relativeTime"
 import { motion } from "framer-motion"
+import { LangfuseWeb } from "langfuse"
+import { GoThumbsdown, GoThumbsup } from "react-icons/go"
 import { RxChatBubble, RxCode } from "react-icons/rx"
 import { useAsyncFn } from "react-use"
 import remarkGfm from "remark-gfm"
@@ -15,6 +17,7 @@ import { Profile } from "@/types/profile"
 import { Api } from "@/lib/api"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader } from "@/components/ui/card"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import {
@@ -41,6 +44,10 @@ import PromptForm from "./prompt-form"
 
 dayjs.extend(relativeTime)
 
+const langfuseWeb = new LangfuseWeb({
+  publicKey: process.env.NEXT_PUBLIC_LANGFUSE_PUBLIC_KEY || "",
+})
+
 function PulsatingCursor() {
   return (
     <motion.div
@@ -60,113 +67,155 @@ function PulsatingCursor() {
 }
 
 export function Message({
+  agent,
   type,
   message,
   profile,
 }: {
+  agent: Agent
   type: string
   message: string
   profile: Profile
 }) {
+  const { toast } = useToast()
+  const handleFeedback = async (value: number) => {
+    await langfuseWeb.score({
+      traceId: agent.id,
+      name: "user-feedback",
+      value,
+      comment: "I like how personalized the response is",
+    })
+
+    toast({
+      description: "Feedback submitted!",
+    })
+  }
+
   return (
-    <div className="min-w-4xl flex max-w-4xl space-x-4 border-b pb-2">
-      <Avatar className="h-8 w-8">
-        <AvatarImage src={type === "ai" ? "/logo.png" : undefined} />
-        <AvatarFallback>
-          {type === "human" &&
-            `${profile.first_name.charAt(0)}${profile.last_name.charAt(0)}`}
-        </AvatarFallback>
-      </Avatar>
-      <div className="ml-4 mt-1 flex-1 space-y-2 overflow-hidden px-1">
-        {message?.length === 0 && <PulsatingCursor />}
-        <MemoizedReactMarkdown
-          className="prose dark:prose-invert prose-p:leading-relaxed prose-pre:p-0 break-words text-sm"
-          remarkPlugins={[remarkGfm, remarkMath]}
-          components={{
-            table({ children }) {
-              return (
-                <div className="mb-2 rounded-md border">
-                  <Table>{children}</Table>
-                </div>
-              )
-            },
-            thead({ children }) {
-              return <TableHeader>{children}</TableHeader>
-            },
-            tbody({ children }) {
-              return <TableBody>{children}</TableBody>
-            },
-            tr({ children }) {
-              return <TableRow>{children}</TableRow>
-            },
-            th({ children }) {
-              return <TableHead className="py-2">{children}</TableHead>
-            },
-            td({ children }) {
-              return <TableCell className="py-2">{children}</TableCell>
-            },
-            p({ children }) {
-              return <p className="mb-5">{children}</p>
-            },
-            a({ children, href }) {
-              return (
-                <a
-                  href={href}
-                  className="text-primary underline"
-                  rel="noreferrer"
-                  target="_blank"
-                >
-                  {children}
-                </a>
-              )
-            },
-            ol({ children }) {
-              return <ol className="mb-5 list-decimal pl-[30px]">{children}</ol>
-            },
-            ul({ children }) {
-              return <ul className="mb-5 list-disc pl-[30px]">{children}</ul>
-            },
-            li({ children }) {
-              return <li className="pb-1">{children}</li>
-            },
-            code({ node, inline, className, children, ...props }) {
-              if (children.length) {
-                if (children[0] == "▍") {
+    <div className="flex flex-col space-y-1 pb-4">
+      <div className="min-w-4xl flex max-w-4xl space-x-4  pb-2">
+        <Avatar className="h-8 w-8">
+          <AvatarImage src={type === "ai" ? "/logo.png" : undefined} />
+          <AvatarFallback>
+            {type === "human" &&
+              `${profile.first_name.charAt(0)}${profile.last_name.charAt(0)}`}
+          </AvatarFallback>
+        </Avatar>
+        <div className="ml-4 mt-1 flex-1 space-y-2 overflow-hidden px-1">
+          {message?.length === 0 && <PulsatingCursor />}
+          <MemoizedReactMarkdown
+            className="prose dark:prose-invert prose-p:leading-relaxed prose-pre:p-0 break-words text-sm"
+            remarkPlugins={[remarkGfm, remarkMath]}
+            components={{
+              table({ children }) {
+                return (
+                  <div className="mb-2 rounded-md border">
+                    <Table>{children}</Table>
+                  </div>
+                )
+              },
+              thead({ children }) {
+                return <TableHeader>{children}</TableHeader>
+              },
+              tbody({ children }) {
+                return <TableBody>{children}</TableBody>
+              },
+              tr({ children }) {
+                return <TableRow>{children}</TableRow>
+              },
+              th({ children }) {
+                return <TableHead className="py-2">{children}</TableHead>
+              },
+              td({ children }) {
+                return <TableCell className="py-2">{children}</TableCell>
+              },
+              p({ children }) {
+                return <p className="mb-5">{children}</p>
+              },
+              a({ children, href }) {
+                return (
+                  <a
+                    href={href}
+                    className="text-primary underline"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    {children}
+                  </a>
+                )
+              },
+              ol({ children }) {
+                return (
+                  <ol className="mb-5 list-decimal pl-[30px]">{children}</ol>
+                )
+              },
+              ul({ children }) {
+                return <ul className="mb-5 list-disc pl-[30px]">{children}</ul>
+              },
+              li({ children }) {
+                return <li className="pb-1">{children}</li>
+              },
+              code({ node, inline, className, children, ...props }) {
+                if (children.length) {
+                  if (children[0] == "▍") {
+                    return (
+                      <span className="mt-1 animate-pulse cursor-default">
+                        ▍
+                      </span>
+                    )
+                  }
+
+                  children[0] = (children[0] as string).replace("`▍`", "▍")
+                }
+
+                const match = /language-(\w+)/.exec(className || "")
+
+                if (inline) {
                   return (
-                    <span className="mt-1 animate-pulse cursor-default">▍</span>
+                    <code
+                      className="light:bg-slate-200 px-1 text-sm dark:bg-slate-800"
+                      {...props}
+                    >
+                      {children}
+                    </code>
                   )
                 }
 
-                children[0] = (children[0] as string).replace("`▍`", "▍")
-              }
-
-              const match = /language-(\w+)/.exec(className || "")
-
-              if (inline) {
                 return (
-                  <code
-                    className="light:bg-slate-200 px-1 text-sm dark:bg-slate-800"
+                  <CodeBlock
+                    key={Math.random()}
+                    language={(match && match[1]) || ""}
+                    value={String(children).replace(/\n$/, "")}
                     {...props}
-                  >
-                    {children}
-                  </code>
+                  />
                 )
-              }
-
-              return (
-                <CodeBlock
-                  key={Math.random()}
-                  language={(match && match[1]) || ""}
-                  value={String(children).replace(/\n$/, "")}
-                  {...props}
-                />
-              )
-            },
-          }}
-        >
-          {message}
-        </MemoizedReactMarkdown>
+              },
+            }}
+          >
+            {message}
+          </MemoizedReactMarkdown>
+        </div>
       </div>
+      {type === "ai" && message.length > 0 && (
+        <div className="flex space-x-2 ">
+          <Button
+            size="sm"
+            variant="outline"
+            onClick={() => handleFeedback(1)}
+            className="rounded-lg"
+          >
+            <GoThumbsup size="15px" />
+          </Button>
+          <Button
+            size="sm"
+            variant="outline"
+            onClick={() => handleFeedback(0)}
+            className="rounded-lg"
+          >
+            <GoThumbsdown size="15px" />
+          </Button>
+        </div>
+      )}
     </div>
   )
 }
@@ -297,9 +346,14 @@ export default function Chat({
         <div className="from-background absolute inset-x-0 top-0 z-20 h-20 bg-gradient-to-b from-0% to-transparent to-50%" />
         {selectedView === "chat" ? (
           <div className="mb-20 mt-10 flex flex-col space-y-5 py-5">
-            <div className="container mx-auto flex max-w-4xl flex-col space-y-5">
+            <div className="container mx-auto flex max-w-4xl flex-col">
               {messages.map(({ type, message }) => (
-                <Message type={type} message={message} profile={profile} />
+                <Message
+                  agent={agent}
+                  type={type}
+                  message={message}
+                  profile={profile}
+                />
               ))}
             </div>
           </div>
@@ -325,6 +379,7 @@ export default function Chat({
                   <CardContent>
                     <ScrollArea className="relative flex max-h-40 grow flex-col rounded-lg border p-3">
                       <Message
+                        agent={agent}
                         type="ai"
                         message={run.outputs?.output}
                         profile={profile}
