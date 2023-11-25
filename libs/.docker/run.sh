385ae82c8a1be1cfc82e0072d6820b8f10bf0973
Andy Ainsworth <heresandyboy@gmail.com>
2023-11-25 18:36:29 +0000

    initial setup for fully local docker setup

--- libs/.docker/run.sh
+++ libs/.docker/run.sh
@@ -1,6 +1,7 @@
 ## To run in docker the backend locally (API, DB, motorhead etc.)
 
-docker compose down && docker-compose --env-file docker.env up --build -d
+docker network create supabase_network_ui
+docker compose down && docker-compose -f docker-compose.api.yml up --build -d
 
 ## This will only run the DB and API in docker
 ## You can then run the UI locally with `npm run dev` and the motorhead in cloud for example
