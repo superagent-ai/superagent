40034ef8ce0148e205bcea10ed99e0173abbc64b
alisalim17 <alisalimli1899@gmail.com>
2023-12-17 01:47:39 +0400

    making input and output optional in workflow step

--- libs/ui/app/workflows/page.tsx
+++ libs/ui/app/workflows/page.tsx
@@ -3,7 +3,9 @@ import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs"
 
 import { Api } from "@/lib/api"
 
-import WorkflowEditor from "./WorkflowEditor"
+import WorkflowEditor from "./[workflowId]/WorkflowEditor"
+import { columns } from "./columns"
+import { DataTable } from "./data-table"
 
 export default async function Workflows() {
   const supabase = createRouteHandlerClient({ cookies })
@@ -18,19 +20,12 @@ export default async function Workflows() {
     .single()
 
   const api = new Api(profile.api_key)
-  const { data } = await api.getAgents()
+  const { data: workflows } = await api.getWorkflows()
 
   return (
-    <div className="flex min-h-full flex-col space-y-4 px-4 py-6">
-      <div className="flex space-x-4">
-        <p className="text-lg">Workflows</p>
-      </div>
-      <div className="flex flex-1 flex-row">
-        <div className="flex flex-1 flex-col items-center justify-center">
-          <WorkflowEditor data={data} />
-        </div>
-        <div className="flex-1">Chat UI</div>
-      </div>
+    <div className="flex flex-col space-y-4 px-4 py-6">
+      <p className="text-lg">Workflows</p>
+      <DataTable columns={columns} data={workflows} profile={profile} />
     </div>
   )
 }
