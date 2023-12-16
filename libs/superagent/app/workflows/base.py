5307700aff528e6d0f78b1557e1c4ec788264412
alisalim17 <alisalimli1899@gmail.com>
2023-12-16 23:02:31 +0400

    adding streaming for workflows

--- libs/superagent/app/workflows/base.py
+++ libs/superagent/app/workflows/base.py
@@ -1,25 +1,43 @@
 from typing import Any
 
 from app.agents.base import AgentBase
-from app.utils.prisma import prisma
+import asyncio
+
+from prisma.models import Workflow
 
 
 class WorkflowBase:
-    def __init__(self, workflow_id: str, enable_streaming: bool = False):
-        self.workflow_id = workflow_id
+    def __init__(self, workflow: Workflow, callbacks:any, enable_streaming: bool = False):
+        self.workflow = workflow
         self.enable_streaming = enable_streaming
 
-    async def arun(self, input: Any) -> Any:
-        workflow = await prisma.workflow.find_unique(
-            where={"id": self.workflow_id},
-            include={"steps": True},
-        )
-        workflow.steps.sort(key=lambda x: x.order)
-        previous_output = input
-        steps_output = {}
-        for step in workflow.steps:
-            agent = await AgentBase(agent_id=step.agentId).get_agent()
-            agent_response = await agent.acall(inputs={"input": previous_output})
-            previous_output = agent_response.get("output")
-            steps_output[step.order] = agent_response
-        return {"steps": steps_output, "output": previous_output}
+        self.callbacks = callbacks
+
+    async def arun(self, input: Any):
+         
+
+            self.workflow.steps.sort(key=lambda x: x.order)
+            previous_output = input
+            steps_output = {}
+            stepIndex = 0
+
+            for step in self.workflow.steps:
+                agent = await AgentBase(
+                    agent_id=step.agentId,
+                    enable_streaming=True,
+                    callback=self.callbacks[stepIndex],
+                ).get_agent()
+
+                task = asyncio.ensure_future(
+                    agent.acall(
+                        inputs={"input": previous_output},
+                    )
+                )
+
+                await task
+                agent_response = task.result()
+                previous_output = agent_response.get("output")
+                steps_output[step.order] = agent_response
+
+                stepIndex += 1
+            return {"steps": steps_output, "output": previous_output}
