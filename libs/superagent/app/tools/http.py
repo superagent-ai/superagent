e49530c4d0570b6639cdc9bc07e8b85349dd9961
ItzCrazyKns <kushagra20103@gmail.com>
2023-12-23 16:02:00 +0530

    Add method validation, exception handling

--- libs/superagent/app/tools/http.py
+++ libs/superagent/app/tools/http.py
@@ -7,27 +7,35 @@ from app.tools.base import BaseTool
 
 class HTTPRequestArgs(BaseModel):
     url: str = Field(..., description="URL for the HTTP request")
-    method: str = Field(default="GET", description="HTTP method like GET or POST")
+    method: str = Field(
+        "GET", description="HTTP method like GET or POST", regex="^(GET|POST)$"
+    )
     headers: dict = Field(default={})
     body: dict = Field(default={})
 
 
 class HTTP(BaseTool):
     name = "HTTP"
-    description = "useful sending post/get requests."
+    description = "Useful for sending HTTP requests."
     return_direct = False
-
     args_schema = HTTPRequestArgs
 
     async def arun(self, args: HTTPRequestArgs) -> dict:
-        async with aiohttp.ClientSession() as session:
-            async with session.request(
-                args.method, args.url, headers=args.headers, json=args.body
-            ) as response:
-                return await response.json()
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.request(
+                    args.method, args.url, headers=args.headers, json=args.body
+                ) as response:
+                    return await response.json()
+        except Exception as e:
+            return {"error": str(e)}
 
     def _run(self, args: HTTPRequestArgs) -> dict:
-        response = requests.request(
-            args.method, args.url, headers=args.headers, json=args.body
-        )
-        return response.json()
+        try:
+            response = requests.request(
+                args.method, args.url, headers=args.headers, json=args.body
+            )
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            return {"error": str(e)}
