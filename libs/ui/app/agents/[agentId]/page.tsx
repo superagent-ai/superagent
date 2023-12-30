1bcbd518505bc62a17fdbc118820644068d5525a
Ismail Pelaseyed <homanp@gmail.com>
2023-12-30 16:22:44 +0100

    Add support for running Azure OpenAI in UI

--- libs/ui/app/agents/[agentId]/page.tsx
+++ libs/ui/app/agents/[agentId]/page.tsx
@@ -21,12 +21,17 @@ export default async function AgentPage({ params }: { params: any }) {
     .eq("user_id", user?.id)
     .single()
   const api = new Api(profile.api_key)
-  const [{ data: agent }, { data: tools }, { data: datasources }] =
-    await Promise.all([
-      api.getAgentById(agentId),
-      api.getTools(),
-      api.getDatasources(),
-    ])
+  const [
+    { data: agent },
+    { data: tools },
+    { data: datasources },
+    { data: llms },
+  ] = await Promise.all([
+    api.getAgentById(agentId),
+    api.getTools(),
+    api.getDatasources(),
+    api.getLLMs(),
+  ])
 
   return (
     <div className="flex h-screen flex-col overflow-hidden">
@@ -35,6 +40,7 @@ export default async function AgentPage({ params }: { params: any }) {
         <Chat agent={agent} profile={profile} />
         <Settings
           agent={agent}
+          configuredLLMs={llms}
           tools={tools}
           profile={profile}
           datasources={datasources}
