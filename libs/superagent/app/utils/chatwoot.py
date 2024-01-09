bc426a3c46b2950a4b11c7675fe8e5c150b20301
GonzaloArray <gonzalo.arrayaran@gmail.com>
2024-01-09 01:12:11 -0300

    fix chatwoot subscription

--- libs/superagent/app/utils/chatwoot.py
+++ libs/superagent/app/utils/chatwoot.py
@@ -4,13 +4,15 @@ from aiohttp import ClientSession
 from os import getenv
 
 CHATWOOT_API_URL = getenv("CHATWOOT") + "accounts/"
+SUBSCRIPTION = getenv("SUBSCRIPTION")
+
 
 async def enviar_respuesta_chatwoot(conversation_id, respuesta, token, account_id, es_respuesta_de_bot=True):
     str_account_id = str(account_id)
     str_conversation_id = str(conversation_id)
     url = f"{CHATWOOT_API_URL}{str_account_id}/conversations/{str_conversation_id}/messages"
 
-    headers = {"Content-Type": "application/json", "api_access_token": token}
+    headers = {"Content-Type": "application/json", "api_access_token": token, 'Ocp-Apim-Subscription-Key': SUBSCRIPTION}
 
     data = {"content": respuesta, "message_type": "outgoing", "private": False}
     # No incluir el sender si la respuesta es del bot
