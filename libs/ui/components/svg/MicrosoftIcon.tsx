12b8cb8f0293de7df94ae4834d6fe678dc3668a7
GonzaloArray <gonzalo.arrayaran@gmail.com>
2024-01-15 23:52:47 -0300

    feat: new sidebar

---
+++ libs/ui/components/svg/MicrosoftIcon.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+
+export const MicrosoftIcon = () => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M8.92857 8.92857L2.86102e-06 8.92857L2.86102e-06 9.53674e-07L8.92857 9.53674e-07L8.92857 8.92857Z"
+        fill="#FF5722"
+      />
+      <path
+        d="M20 8.92857L11.0714 8.92857L11.0714 9.53674e-07L20 9.53674e-07V8.92857Z"
+        fill="#4CAF50"
+      />
+      <path d="M20 20H11.0714L11.0714 11.0714L20 11.0714V20Z" fill="#FFC107" />
+      <path
+        d="M8.92857 20H2.86102e-06L2.86102e-06 11.0714L8.92857 11.0714L8.92857 20Z"
+        fill="#03A9F4"
+      />
+    </svg>
+  )
+}
