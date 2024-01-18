f988aa3ec04f05ab11de6f3c96a5e74a1abf659e
Ismail Pelaseyed <homanp@gmail.com>
2024-01-18 13:45:52 -0800

    Add tabs to agent detail

--- libs/ui/app/agents/agent.tsx
+++ libs/ui/app/agents/agent.tsx
@@ -1,10 +1,12 @@
 "use client"
 
 import { useEffect } from "react"
+import { RxActivityLog, RxGear, RxPlay } from "react-icons/rx"
 import { useAsyncFn } from "react-use"
 
 import { Api } from "@/lib/api"
 import { Badge } from "@/components/ui/badge"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 interface Agent {
   agent: any
@@ -19,16 +21,14 @@ export default function Agent({ agent, profile }: Agent) {
   const [{ loading, value }, fetch] = useAsyncFn(async () => {
     const { data: runs } = await api.getRuns({ agentId: agent.id })
     return runs
-  }, [api, agent])
+  }, [])
 
   useEffect(() => {
     fetch()
-  }, [fetch])
-
-  console.log(agent)
+  }, [agent, fetch])
 
   return (
-    <div className="flex flex-col space-y-5 p-6">
+    <div className="flex flex-1 flex-col space-y-5 p-6">
       <div className="flex space-x-2 text-sm text-muted-foreground">
         <span>Assistants</span>
         <span>/</span>
@@ -56,6 +56,27 @@ export default function Agent({ agent, profile }: Agent) {
           </span>
         </div>
       </div>
+      <Tabs defaultValue="overview">
+        <TabsList>
+          <TabsTrigger value="overview" className="space-x-1">
+            <RxGear size={12} />
+            <span>OVERVIEW</span>
+          </TabsTrigger>
+          <TabsTrigger value="logs" className="space-x-1">
+            <RxActivityLog size={12} />
+            <span>LOGS</span>
+          </TabsTrigger>
+          <TabsTrigger value="run" className="space-x-1">
+            <RxPlay size={12} />
+            <span>RUN</span>
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="overview" className="p-2">
+          Overview
+        </TabsContent>
+        <TabsContent value="logs">Logs</TabsContent>
+        <TabsContent value="runs">Runs</TabsContent>
+      </Tabs>
     </div>
   )
 }
