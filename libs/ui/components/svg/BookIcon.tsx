9ec128a332aa0905acc81ae559e58aeda593f8b8
GonzaloArray <gonzalo.arrayaran@gmail.com>
2024-01-19 21:51:23 -0300

    Feat - SVG icon

---
+++ libs/ui/components/svg/BookIcon.tsx
@@ -0,0 +1,38 @@
+import React from "react"
+
+export const BookIcon = () => {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M5.3335 2H1.3335V12H6.00016C7.1335 12 8.00016 12.8667 8.00016 14V4.66667C8.00016 3.2 6.80016 2 5.3335 2Z"
+        stroke="white"
+        stroke-opacity="0.75"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M10.6665 8.00002L11.9998 9.33335L14.6665 6.66669"
+        stroke="white"
+        stroke-opacity="0.75"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M14.6667 4V2H10.6667C9.2 2 8 3.2 8 4.66667V14C8 12.8667 8.86667 12 10 12H14.6667V10.4667"
+        stroke="white"
+        stroke-opacity="0.75"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  )
+}
