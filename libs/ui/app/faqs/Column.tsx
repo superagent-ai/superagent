a3df144756f4541a678b840dffb49fe0ae2f95e2
GonzaloArray <gonzalo.arrayaran@gmail.com>
2024-01-19 21:37:27 -0300

    feat: faqs column ui

---
+++ libs/ui/app/faqs/Column.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { ItemFA } from './components/ItemFA'
+
+export const Column = () => {
+  return (
+    <div className='flex flex-col gap-4'>
+      <ItemFA description='Direccion del deposito' id='asdfsdaf'/>
+      <ItemFA description='Direccion del deposito' id='asdfsdaf'/>
+      <ItemFA description='Direccion del deposito' id='asdfsdaf'/>
+      <ItemFA description='Direccion del deposito' id='asdfsdaf'/>
+      <ItemFA description='Direccion del deposito' id='asdfsdaf'/>
+
+    </div>
+  )
+}
