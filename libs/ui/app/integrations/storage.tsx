1aacb37375aed8689ac9c91b0a895f0f5ca4da02
Ismail Pelaseyed <homanp@gmail.com>
2024-01-21 16:18:38 -0800

    Add the possiblity to update/create new storage connection

--- libs/ui/app/integrations/storage.tsx
+++ libs/ui/app/integrations/storage.tsx
@@ -1,28 +1,127 @@
 "use client"
 
+import * as React from "react"
 import Image from "next/image"
-import { TbCheck } from "react-icons/tb"
+import { useRouter } from "next/navigation"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
 import { useAsync } from "react-use"
+import * as z from "zod"
 
 import { siteConfig } from "@/config/site"
 import { Api } from "@/lib/api"
 import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 import { Skeleton } from "@/components/ui/skeleton"
+import { Spinner } from "@/components/ui/spinner"
+
+const pineconeSchema = z.object({
+  PINECONE_API_KEY: z.string(),
+  PINECONE_ENVIRONMENT: z.string(),
+  PINECONE_INDEX: z.string(),
+})
+
+const qdrantSchema = z.object({
+  QDRANT_API_KEY: z.string(),
+  QDRANT_HOST: z.string(),
+  QDRANT_INDEX: z.string(),
+})
+
+const astraDbSchema = z.object({
+  ASTRA_DB_APPLICATION_TOKEN: z.string(),
+  ASTRA_DB_REGION: z.string(),
+  ASTRA_DB_ID: z.string(),
+  ASTRA_DB_COLLECTION_NAME: z.string(),
+  ASTRA_DB_KEYSPACE_NAME: z.string(),
+})
+
+const weaviateSchema = z.object({
+  WEAVIATE_API_KEY: z.string(),
+  WEAVIATE_URL: z.string(),
+  WEAVIATE_INDEX: z.string(),
+})
+
+const supabasePgVectorSchema = z.object({
+  SUPABASE_DB_URL: z.string(),
+  SUPABASE_TABLE_NAME: z.string(),
+})
+
+const formSchema = z.object({
+  options: z.union([
+    pineconeSchema,
+    qdrantSchema,
+    astraDbSchema,
+    weaviateSchema,
+    supabasePgVectorSchema,
+  ]),
+})
 
 function Loading() {
   return (
     <div className="flex flex-col space-y-4">
-      {Array(5).fill(<Skeleton className="h-[80px] w-full" />)}
+      {Array(5).fill(<Skeleton className="h-[60px] w-full" />)}
     </div>
   )
 }
 
 export default function Storage({ profile }: { profile: any }) {
+  const [open, setOpen] = React.useState<boolean>()
+  const [selectedDB, setSelectedDB] = React.useState<any>()
+  const router = useRouter()
   const api = new Api(profile.api_key)
+  const { ...form } = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      options: {},
+    },
+  })
   const { value: configuredDBs, loading } = useAsync(async () => {
     const { data } = await api.getVectorDbs()
     return data
   }, [])
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    const payload = {
+      ...values,
+      options:
+        Object.keys(values.options).length === 0 ? undefined : values.options,
+    }
+
+    const isExistingConnection = configuredDBs.find(
+      (db: any) => db.provider === selectedDB.provider
+    )
+
+    if (isExistingConnection) {
+      await api.patchVectorDb(isExistingConnection.id, {
+        ...payload,
+        provider: selectedDB.provider,
+      })
+    } else {
+      await api.createVectorDb({ ...payload, provider: selectedDB.provider })
+    }
+
+    form.reset()
+    router.refresh()
+    setOpen(false)
+  }
 
   return (
     <div className="container flex max-w-4xl flex-col space-y-10 pt-10">
@@ -63,7 +162,14 @@ export default function Storage({ profile }: { profile: any }) {
                     <p className="font-medium">{vectorDb.name}</p>
                   </div>
                 </div>
-                <Button variant="outline" size="sm">
+                <Button
+                  variant="outline"
+                  size="sm"
+                  onClick={() => {
+                    setSelectedDB(vectorDb)
+                    setOpen(true)
+                  }}
+                >
                   Settings
                 </Button>
               </div>
@@ -71,6 +177,81 @@ export default function Storage({ profile }: { profile: any }) {
           })}
         </div>
       )}
+      <Dialog
+        onOpenChange={(isOpen) => {
+          if (!isOpen) {
+            form.reset()
+          }
+
+          setOpen(isOpen)
+        }}
+        open={open}
+      >
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>{selectedDB?.name}</DialogTitle>
+            <DialogDescription>
+              Connect your private {selectedDB?.name} account to Superagent.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="flex flex-col">
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="w-full space-y-4"
+              >
+                {selectedDB?.metadata.map((metadataField: any) => (
+                  <FormField
+                    key={metadataField.key}
+                    control={form.control}
+                    // @ts-ignore
+                    name={`options.${metadataField.key}`}
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>{metadataField.label}</FormLabel>
+                        {metadataField.type === "input" && (
+                          <FormControl>
+                            {/* @ts-ignore */}
+                            <Input
+                              {...field}
+                              placeholder={
+                                "placeholder" in metadataField
+                                  ? metadataField.placeholder
+                                  : ""
+                              }
+                              type="text"
+                            />
+                          </FormControl>
+                        )}
+                        {"helpText" in metadataField && (
+                          <FormDescription className="pb-2">
+                            {metadataField.helpText as string}
+                          </FormDescription>
+                        )}
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                ))}
+                <DialogFooter>
+                  <DialogClose asChild>
+                    <Button type="button" variant="ghost">
+                      Close
+                    </Button>
+                  </DialogClose>
+                  <Button type="submit" size="sm">
+                    {form.control._formState.isSubmitting ? (
+                      <Spinner />
+                    ) : (
+                      "Save configuration"
+                    )}
+                  </Button>
+                </DialogFooter>
+              </form>
+            </Form>
+          </div>
+        </DialogContent>
+      </Dialog>
     </div>
   )
 }
