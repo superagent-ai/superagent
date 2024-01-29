0236fc7c95981e815dfdbbf4dd3e4f1b4b4b3753
Ali Salimli <67149699+elisalimli@users.noreply.github.com>
2024-01-29 21:45:44 +0400

    fix code block background color (#721)
    * Add workflow list page
    
    * Add support for fetching stats for workflows
    
    * Refactor routing
    
    * Adapt the chat interface to make requests to workflows endpoint
    
    * Minor additions
    
    * Fix bigquery connection
    
    * fix code block background color
    
    * Small tweaks
    
    ---------
    
    Co-authored-by: Ismail Pelaseyed <homanp@gmail.com>

--- libs/ui/components/codeblock.tsx
+++ libs/ui/components/codeblock.tsx
@@ -126,7 +126,6 @@ const CodeBlock: FC<Props> = memo(({ language, value }) => {
         customStyle={{
           margin: 0,
           width: "100%",
-          background: "transparent",
           padding: "1.5rem 1rem",
         }}
         codeTagProps={{
