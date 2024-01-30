6a71362617e9bdd0901aa315e0bdba2ef31438d3
Ali Salimli <67149699+elisalimli@users.noreply.github.com>
2024-01-30 11:42:50 +0400

    Listing Intermediate Steps (#719)
    * Add workflow list page
    
    * Add support for fetching stats for workflows
    
    * Refactor routing
    
    * Adapt the chat interface to make requests to workflows endpoint
    
    * Minor additions
    
    * Fix bigquery connection
    
    * add resizable component
    
    * add listing intermediate steps
    
    * refactor ui
    
    * update default panel size
    
    * Small tweaks
    
    * Minor style fixes
    
    * Add workflow list page
    
    * Adapt the chat interface to make requests to workflows endpoint
    
    * Minor additions
    
    * add resizable component
    
    * add listing intermediate steps
    
    * refactor ui
    
    * update default panel size
    
    * Small tweaks
    
    * Minor tweaks
    
    ---------
    
    Co-authored-by: Ismail Pelaseyed <homanp@gmail.com>

--- libs/ui/app/workflows/[id]/chat.tsx
+++ libs/ui/app/workflows/[id]/chat.tsx
@@ -8,15 +8,21 @@ import relativeTime from "dayjs/plugin/relativeTime"
 import { v4 as uuidv4 } from "uuid"
 
 import { Profile } from "@/types/profile"
-import { Api } from "@/lib/api"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { useToast } from "@/components/ui/use-toast"
 import Message from "@/components/message"
 
+import FunctionCalls from "./function-calls"
 import LLMDialog from "./llm-dialog"
 import PromptForm from "./prompt-form"
 
 dayjs.extend(relativeTime)
+
+const defaultFunctionCalls = [
+  {
+    type: "start",
+  },
+]
 
 export default function Chat({
   workflow,
@@ -39,6 +45,9 @@ export default function Chat({
       ? [{ type: "ai", message: workflowSteps[0].agent.initialMessage }]
       : []
   )
+
+  const [functionCalls, setFunctionCalls] = React.useState<any[]>()
+
   const [timer, setTimer] = React.useState<number>(0)
   const [session, setSession] = React.useState<string | null>(uuidv4())
   const [open, setOpen] = React.useState<boolean>(false)
@@ -47,15 +56,20 @@ export default function Chat({
 
   const abortControllerRef = React.useRef<AbortController | null>(null)
 
+  const resetState = () => {
+    setIsLoading(false)
+    setTimer(0)
+    if (timerRef.current) {
+      clearInterval(timerRef.current)
+    }
+  }
+
   const abortStream = () => {
     if (abortControllerRef.current) {
       abortControllerRef.current.abort()
-      setIsLoading(false)
-      setTimer(0)
-      if (timerRef.current) {
-        clearInterval(timerRef.current)
-      }
     }
+
+    resetState()
   }
 
   async function onSubmit(value?: string) {
@@ -102,15 +116,32 @@ export default function Chat({
           }),
           openWhenHidden: true,
           signal: abortControllerRef.current.signal,
+          async onopen() {
+            setFunctionCalls(defaultFunctionCalls)
+          },
           async onclose() {
-            setIsLoading(false)
-            setTimer(0)
-            if (timerRef.current) {
-              clearInterval(timerRef.current)
-            }
+            console.log("close")
+            setFunctionCalls((previousFunctionCalls = []) => [
+              ...previousFunctionCalls,
+              {
+                type: "end",
+              },
+            ])
+            resetState()
           },
           async onmessage(event) {
             if (event.id) currentEventId = event.id
+
+            if (event.event === "function_call") {
+              const data = JSON.parse(event.data)
+              setFunctionCalls((previousFunctionCalls = []) => [
+                ...previousFunctionCalls,
+                {
+                  ...data,
+                  type: "function_call",
+                },
+              ])
+            }
 
             if (
               event.data !== "[END]" &&
@@ -143,11 +174,7 @@ export default function Chat({
         }
       )
     } catch {
-      setIsLoading(false)
-      setTimer(0)
-      if (timerRef.current) {
-        clearInterval(timerRef.current)
-      }
+      resetState()
       setMessages((previousMessages) => {
         let updatedMessages = [...previousMessages]
 
@@ -175,53 +202,54 @@ export default function Chat({
   }, [messages])
 
   return (
-    <div className="relative flex h-full w-full flex-[60%] bg-background text-sm">
-      <div className="absolute inset-x-0 top-0 z-50 flex items-center justify-between px-6 py-4">
-        <p
-          className={`${
-            timer === 0 ? "text-muted-foreground" : "text-primary"
-          } font-mono text-sm`}
-        >
-          {timer.toFixed(1)}s
-        </p>
-      </div>
-      <ScrollArea className="w-[100%]">
-        <div className="mx-auto mb-20 flex max-w-4xl flex-1 flex-col space-y-0 px-4 py-12">
-          {messages.map(({ type, message, steps }, index) => (
-            <Message
-              key={index}
-              type={type}
-              message={message}
-              steps={steps}
-              profile={profile}
-            />
-          ))}
+    <div className="h-full">
+      <div className="relative flex h-full w-full bg-background text-sm">
+        <div className="mt-12 flex-[20%] px-6">
+          <p
+            className={`${
+              timer === 0 ? "text-muted-foreground" : "text-primary"
+            } mb-4 font-mono text-sm`}
+          >
+            {timer.toFixed(1)}s
+          </p>
+          <FunctionCalls functionCalls={functionCalls} />
         </div>
-      </ScrollArea>
-      <div className="absolute inset-x-0 bottom-10 z-50 h-[100px] bg-gradient-to-t from-muted from-0% to-transparent to-50%">
-        <div className="relative mx-auto max-w-2xl px-8">
-          <PromptForm
-            onStop={() => abortStream()}
-            onSubmit={async (value) => {
-              if (llms.length === 0) {
-                setOpen(true)
-                return
-              }
 
-              onSubmit(value)
-            }}
-            onCreateSession={async (uuid) => {
-              setSession(uuid)
-              if (timerRef.current) {
-                clearInterval(timerRef.current)
-              }
-              setMessages([])
-              toast({
-                description: "New session created",
-              })
-            }}
-            isLoading={isLoading}
-          />
+        <div className="relative flex h-full flex-[80%] text-sm">
+          <ScrollArea className="w-full pr-20">
+            <div className="mb-20 flex max-w-4xl flex-1 flex-col space-y-0 px-4 py-12">
+              {messages.map(({ type, message, steps }, index) => (
+                <Message
+                  key={index}
+                  type={type}
+                  message={message}
+                  steps={steps}
+                  profile={profile}
+                />
+              ))}
+            </div>
+          </ScrollArea>
+        </div>
+        <div className="absolute inset-x-0 bottom-10 z-50 h-[100px] bg-gradient-to-t from-muted from-0% to-transparent to-50%">
+          <div className="relative mx-auto max-w-2xl px-8">
+            <PromptForm
+              onStop={() => abortStream()}
+              onSubmit={async (value) => {
+                onSubmit(value)
+              }}
+              onCreateSession={async (uuid) => {
+                setSession(uuid)
+                if (timerRef.current) {
+                  clearInterval(timerRef.current)
+                }
+                setMessages([])
+                toast({
+                  description: "New session created",
+                })
+              }}
+              isLoading={isLoading}
+            />
+          </div>
         </div>
       </div>
       <LLMDialog
