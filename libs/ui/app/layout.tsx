a2e8c690ec48b31c45f1e0f419b4c7142cd7fa85
GonzaloArray <gonzalo.arrayaran@gmail.com>
2024-01-30 17:13:51 -0300

    feat new logo

--- libs/ui/app/layout.tsx
+++ libs/ui/app/layout.tsx
@@ -22,8 +22,8 @@ export const metadata: Metadata = {
   ],
   icons: {
     icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
+    shortcut: "/android-chrome-192x192.png",
+    apple: "/android-chrome-192x192.png",
   },
 }
 
