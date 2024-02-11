cdcf1c9f14453ee970009850e071bae000821c7d
Ismail Pelaseyed <homanp@gmail.com>
2024-02-11 11:52:04 -0800

    Add coming soon cards to stats and logs pages

--- libs/ui/app/logs/page.tsx
+++ libs/ui/app/logs/page.tsx
@@ -1,5 +1,6 @@
 import { cookies } from "next/headers"
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs"
+import { TbTerminal2 } from "react-icons/tb"
 
 import { Api } from "@/lib/api"
 import LogList from "@/components/log-list"
@@ -32,7 +33,18 @@ export default async function Agents({
     <div className="flex h-screen flex-col justify-between space-y-0 overflow-hidden">
       <p className="border-b px-6 py-4 font-medium">Logs</p>
       <div className="flex grow overflow-auto">
-        <LogList data={logs} profile={profile} />
+        {/*<LogList data={logs} profile={profile} /> */}
+        <div className="flex h-full w-full items-center justify-center ">
+          <div className="container flex max-w-lg flex-col space-y-6 rounded-lg border p-6">
+            <TbTerminal2 fontSize="30px" />
+            <div className="flex flex-col space-y-2">
+              <p className="text-sm font-semibold">Coming soon</p>
+              <p className="text-sm text-muted-foreground">
+                We will be rolling out detailed logs for your workflows soon!
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
