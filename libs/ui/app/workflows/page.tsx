c4c4fb8f475a18ae756887a9f9e6ffccf0517177
Ismail Pelaseyed <homanp@gmail.com>
2024-02-12 14:32:29 -0800

    Remove alert

--- libs/ui/app/workflows/page.tsx
+++ libs/ui/app/workflows/page.tsx
@@ -4,7 +4,6 @@ import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs"
 import { TbAlertTriangle } from "react-icons/tb"
 
 import { Api } from "@/lib/api"
-import { Alert, AlertDescription } from "@/components/ui/alert"
 
 import WorkflowCards from "./cards"
 import Header from "./header"
@@ -25,28 +24,9 @@ export default async function Agents() {
 
   const { data: workflows } = await api.getWorkflows()
 
-  const showAlert = new Date(profile.created_at) < new Date("2024-02-07")
-
   return (
     <div className="flex h-screen w-full flex-col justify-between space-y-4 overflow-hidden">
       <Header profile={profile} />
-      {showAlert && (
-        <div className="px-6">
-          <Alert>
-            <AlertDescription className=" flex items-center space-x-4 font-semibold ">
-              <TbAlertTriangle fontSize="20px" className="text-orange-400" />
-              <p>
-                Heads up! We are replacing the legacy agents view with
-                workflows. The legacy view can still be{" "}
-                <Link href="/agents" className="underline">
-                  accessed here
-                </Link>{" "}
-                for a limited time while we migrate your agents.
-              </p>
-            </AlertDescription>
-          </Alert>
-        </div>
-      )}
       <WorkflowCards workflows={workflows} />
     </div>
   )
