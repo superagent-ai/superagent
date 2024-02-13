48605fb362d20718bd62b38985a34e292eaf6ea8
Ismail Pelaseyed <homanp@gmail.com>
2024-02-12 22:15:32 -0800

    Add support for perplexity models

--- libs/superagent/app/agents/llm.py
+++ libs/superagent/app/agents/llm.py
@@ -11,13 +11,29 @@ from app.agents.base import AgentBase
 class LLMAgent(AgentBase):
     async def get_agent(self):
         enable_streaming = self.enable_streaming
+        agent_config = self.agent_config
+        print(agent_config)
 
-        class CustomAgentExecutor(AgentExecutor):
+        class CustomAgentExecutor:
             async def ainvoke(self, *args, **kwargs):
-                print(*args)
-                print(**kwargs)
-
-                return None
+                input = kwargs.get("input")
+                model = agent_config.metadata["model"]
+                prompt = agent_config.prompt
+                api_key = agent_config.llms[0].llm.apiKey
+                res = await acompletion(
+                    model=model,
+                    api_key=api_key,
+                    messages=[
+                        {"content": prompt, "role": "system"},
+                        {"content": input, "role": "user"},
+                    ],
+                )
+                return {
+                    "input": input,
+                    "output": res.choices[0].message.content,
+                    "completion_tokens": res.usage.completion_tokens,
+                    "prompt_tokens": res.usage.prompt_tokens,
+                }
 
         agent_executor = CustomAgentExecutor()
 
