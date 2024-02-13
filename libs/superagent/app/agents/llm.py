793f08728f2dc82310f5518f6216f4462fe822c5
Ismail Pelaseyed <homanp@gmail.com>
2024-02-13 08:50:25 -0800

    Add streaming support for OSS LLMs (#815)
    * Add streaming support for OSS LLMs
    
    * Add support for streaming
    
    * Fix linting

--- libs/superagent/app/agents/llm.py
+++ libs/superagent/app/agents/llm.py
@@ -1,6 +1,3 @@
-import asyncio
-
-from langchain.agents import AgentExecutor
 from langchain.schema.messages import AIMessage
 from langchain.schema.output import ChatGeneration, LLMResult
 from litellm import acompletion
@@ -16,23 +13,49 @@ class LLMAgent(AgentBase):
 
         class CustomAgentExecutor:
             async def ainvoke(self, *args, **kwargs):
-                input = kwargs.get("input")
-                model = agent_config.metadata["model"]
+                input = args[0]
+                model = agent_config.metadata.get("model", "gpt-3.5-turbo")
                 prompt = agent_config.prompt
                 api_key = agent_config.llms[0].llm.apiKey
+
                 res = await acompletion(
-                    model=model,
                     api_key=api_key,
+                    model=model,
                     messages=[
                         {"content": prompt, "role": "system"},
                         {"content": input, "role": "user"},
                     ],
+                    stream=enable_streaming,
                 )
+
+                output = ""
+                if enable_streaming:
+                    streaming = kwargs["config"]["callbacks"][0]
+                    await streaming.on_llm_start()
+
+                    async for chunk in res:
+                        token = chunk.choices[0].delta.content
+                        if token:
+                            output += token
+                            await streaming.on_llm_new_token(token)
+
+                    await streaming.on_llm_end(
+                        response=LLMResult(
+                            generations=[
+                                [
+                                    ChatGeneration(
+                                        message=AIMessage(
+                                            content=output,
+                                        )
+                                    )
+                                ]
+                            ],
+                        )
+                    )
+
                 return {
                     "input": input,
-                    "output": res.choices[0].message.content,
-                    "completion_tokens": res.usage.completion_tokens,
-                    "prompt_tokens": res.usage.prompt_tokens,
+                    "output": output,
                 }
 
         agent_executor = CustomAgentExecutor()
