01d115eac6ee5df82133ecfcb7b5565518aeaa87
Ismail Pelaseyed <homanp@gmail.com>
2024-02-17 14:27:53 -0800

    Small tweaks

--- libs/superagent/services/superrag.py
+++ libs/superagent/services/superrag.py
@@ -4,6 +4,14 @@ import aiohttp
 import requests
 from decouple import config
 
+ENCODER = {
+    "encoder": {
+        "type": "openai",
+        "name": "text-embedding-3-small",
+        "dimensions": 1536,
+    }
+}
+
 
 class SuperRagService:
     def __init__(self, url: Optional[str] = None):
@@ -23,10 +31,10 @@ class SuperRagService:
         return requests.request(method, f"{self.url}/{endpoint}", json=data).json()
 
     async def aingest(self, data):
-        return await self._arequest("POST", "ingest", data)
+        return await self._arequest("POST", "ingest", {**data, **ENCODER})
 
     async def adelete(self, data):
         return await self._arequest("DELETE", "delete", data)
 
     def query(self, data):
-        return self._request("POST", "query", data)
+        return self._request("POST", "query", {**data, **ENCODER})
