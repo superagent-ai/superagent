9bcec8d946cfe216f9ccad8eaafd192a2582b5ba
alisalim17 <alisalimli1899@gmail.com>
2024-03-11 20:46:08 +0400

    chore: remove arequest

--- libs/superagent/services/superrag.py
+++ libs/superagent/services/superrag.py
@@ -1,6 +1,5 @@
 from typing import Optional
 
-import aiohttp
 import requests
 from decouple import config
 
@@ -12,25 +11,18 @@ class SuperRagService:
         if not self.url:
             raise ValueError("SUPERRAG_API_URL is not set")
 
-    async def _arequest(self, method, endpoint, data):
-        async with aiohttp.ClientSession() as session:
-            async with session.request(
-                method, f"{self.url}/{endpoint}", json=data
-            ) as response:
-                return await response.json()
-
     def _request(self, method, endpoint, data):
         return requests.request(method, f"{self.url}/{endpoint}", json=data).json()
 
-    async def aingest(self, data):
-        return await self._arequest(
+    def ingest(self, data):
+        return self._request(
             "POST",
             "ingest",
             data,
         )
 
-    async def adelete(self, data):
-        return await self._arequest("DELETE", "delete", data)
+    def delete(self, data):
+        return self._request("DELETE", "delete", data)
 
     def query(self, data):
         return self._request(
