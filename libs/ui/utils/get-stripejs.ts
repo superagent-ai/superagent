322f88f6e1b4407e3434471f0d283ad46a0695d4
alisalim17 <alisalimli1899@gmail.com>
2024-03-25 17:22:05 +0400

    feat: add helper function initialize stripe js

---
+++ libs/ui/utils/get-stripejs.ts
@@ -0,0 +1,11 @@
+import { loadStripe, Stripe } from "@stripe/stripe-js"
+
+let stripePromise: Promise<Stripe | null>
+const getStripe = () => {
+  if (!stripePromise) {
+    stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!)
+  }
+  return stripePromise
+}
+
+export default getStripe
