b46d8de28004ca776d3292cf6693507ed52ea99c
Ismail Pelaseyed <homanp@gmail.com>
2024-03-26 16:27:15 -0700

    Remove billing modal

--- libs/ui/app/container.tsx
+++ libs/ui/app/container.tsx
@@ -1,7 +1,5 @@
 import Sidebar from "@/components/sidebar"
 
-import BillingModal from "./billing-modal"
-
 interface RootLayoutProps {
   children: React.ReactNode
   profile: any
@@ -10,9 +8,11 @@ interface RootLayoutProps {
 export default function RootLayout({ children, profile }: RootLayoutProps) {
   return (
     <section className="flex h-screen">
+      {/*
       {process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY && (
         <BillingModal profile={profile} />
       )}
+      */}
       <Sidebar />
       <div className="flex-1 overflow-auto">{children}</div>
     </section>
