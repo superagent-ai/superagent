a1f450792a95867757a0bca58739b0a32934611f
alisalim17 <alisalimli1899@gmail.com>
2024-03-30 21:15:53 +0400

    feat: refactor http tool

--- libs/superagent/app/tools/http.py
+++ libs/superagent/app/tools/http.py
@@ -1,4 +1,5 @@
 import json
+import logging
 
 import aiohttp
 import requests
@@ -9,48 +10,47 @@ from app.utils.helpers import get_first_non_null
 supported_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
 
+logger = logging.getLogger(__name__)
+
+
 class LCHttpTool(LCBaseTool):
     name = "API Request"
     description = "useful for making GET/POST API requests"
     return_direct = False
 
     def _run(self, body: dict = None) -> None:
-        headers = self.metadata.get("headers")
-        if isinstance(headers, str):
-            headers = json.loads(headers)
-        elif headers is None:
-            headers = {}
+        headers = self.metadata.get("headers") or "{}"
+        headers = json.loads(headers)
         headers["content-type"] = "application/json"
 
+        url = get_first_non_null(
+            self.metadata.get("url"),
+            self.metadata.get("defaultURL"),
+        )
+
+        method = get_first_non_null(
+            self.metadata.get("method"),
+            self.metadata.get("defaultMethod"),
+        )
+        if method not in supported_methods:
+            logger.warning(f"Method {method} not supported. Defaulting to GET method")
+            method = "GET"
+
+        if body is None:
+            body = self.metadata.get("defaultBody", {})
+
+        request_kwargs = {"method": method, "url": url, "headers": headers}
+        if body is not None:
+            request_kwargs["json"] = body
+
         try:
-            url = get_first_non_null(
-                self.metadata.get("url"),
-                self.metadata.get("defaultURL"),
-            )
-
-            method = get_first_non_null(
-                self.metadata.get("method"),
-                self.metadata.get("defaultMethod"),
-            )
-            if method not in supported_methods:
-                method = self.metadata.get("defaultMethod", "GET")
-
-            if body is None:
-                body = self.metadata.get("defaultBody", {})
-
-            request_kwargs = {"method": method, "url": url, "headers": headers}
-            if body is not None:
-                request_kwargs["json"] = body
             response = requests.request(**request_kwargs)
             response.raise_for_status()
-            try:
-                return response.json()
-            except Exception:
-                return "Request successful"
-        except requests.exceptions.RequestException as e:
-            return str(e)
+            return response.json()
+        except Exception as e:
+            return f"Request failed: {str(e)}"
 
-    async def _arun(self, body: dict = None) -> str:
+    async def _arun(self, body: dict = None) -> None:
         headers = self.metadata.get("headers")
         if isinstance(headers, str):
             headers = json.loads(headers)
@@ -68,21 +68,21 @@ class LCHttpTool(LCBaseTool):
             self.metadata.get("defaultMethod"),
         )
 
+        if method not in supported_methods:
+            logger.warning(f"Method {method} not supported. Defaulting to GET method")
+            method = "GET"
+
+        if body is None:
+            body = self.metadata.get("defaultBody", {})
+
+        request_kwargs = {"method": method, "url": url, "headers": headers}
+        if body is not None:
+            request_kwargs["json"] = body
+
         try:
-            if method not in supported_methods:
-                method = self.metadata.get("defaultMethod", "GET")
-
-            if body is None:
-                body = self.metadata.get("defaultBody", {})
-
             async with aiohttp.ClientSession() as session:
-                request_kwargs = {"method": method, "url": url, "headers": headers}
-                if body is not None:
-                    request_kwargs["json"] = body
                 async with session.request(**request_kwargs) as response:
-                    try:
-                        return await response.json()
-                    except Exception:
-                        return "Request successfull"
+                    response.raise_for_status()
+                    return await response.json()
         except Exception as e:
-            return str(e)
+            return f"Request failed: {str(e)}"
