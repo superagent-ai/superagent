b5f6ed2cc6d3db17c191141f62363806e1a33e5a
Ali Salimli <67149699+elisalimli@users.noreply.github.com>
2024-04-09 23:13:49 +0400

    fix: throwing segment logging error (#944)

--- libs/superagent/app/utils/analytics.py
+++ libs/superagent/app/utils/analytics.py
@@ -1,3 +1,5 @@
+import logging
+
 import segment.analytics as analytics
 from decouple import config
 from pydantic import BaseModel
@@ -6,6 +8,8 @@ from prisma.models import Agent
 
 SEGMENT_WRITE_KEY = config("SEGMENT_WRITE_KEY", None)
 analytics.write_key = SEGMENT_WRITE_KEY
+
+logger = logging.getLogger(__name__)
 
 
 class AgentInvocationData(BaseModel):
@@ -57,24 +61,5 @@ def track_agent_invocation(data: AgentInvocationData):
                 "completion_tokens_cost_usd": data.get("completion_tokens_cost_usd", 0),
             },
         )
-    except Exception:
-        analytics.track(
-            data["user_id"],
-            "Invoked Agent",
-            {
-                "agentId": data.get("agent", {}).id,
-                "workflowId": data.get("workflow_id", None),
-                "llm_model": data.get("agent", {}).llmModel,
-                "sessionId": data["session_id"],
-                "response": {
-                    "status_code": data.get("status_code", 200),
-                    "error": data.get("error", None),
-                },
-                "output": data.get("output", None),
-                "input": data.get("input", None),
-                "prompt_tokens": data.get("prompt_tokens", 0),
-                "completion_tokens": data.get("completion_tokens", 0),
-                "prompt_tokens_cost_usd": data.get("prompt_tokens_cost_usd", 0),
-                "completion_tokens_cost_usd": data.get("completion_tokens_cost_usd", 0),
-            },
-        )
+    except Exception as e:
+        logger.error(f"Error tracking agent invocation: {e}")
