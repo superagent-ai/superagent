424ce477d3fc6f75fc2fb4b99c1da18d7fbc597c
Ismail Pelaseyed <homanp@gmail.com>
2026-01-19 12:38:27 +0100

    fix: remove deleted scan prompt export

--- sdk/typescript/src/prompts/index.ts
+++ sdk/typescript/src/prompts/index.ts
@@ -1,3 +1,2 @@
 export { GUARD_SYSTEM_PROMPT, buildGuardUserMessage } from "./guard.js";
 export { REDACT_SYSTEM_PROMPT, buildRedactUserMessage } from "./redact.js";
-export { SCAN_SYSTEM_PROMPT, buildScanUserMessage } from "./scan.js";
